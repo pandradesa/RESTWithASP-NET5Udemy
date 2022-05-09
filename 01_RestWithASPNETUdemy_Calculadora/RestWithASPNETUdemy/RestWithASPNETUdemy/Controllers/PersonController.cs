0f0171541194fa9fd3e4f59543ca29d77bf2674d
Paula Sá <pc.andrade.sa@gmail.com>
2022-05-09 14:53:10 -0300

    working with four verbs

--- 01_RestWithASPNETUdemy_Calculadora/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ 01_RestWithASPNETUdemy_Calculadora/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -4,28 +4,22 @@ namespace RestWithASPNETUdemy.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CalculatorController : ControllerBase
+    public class PersonController : ControllerBase
     {
 
-        private readonly ILogger<CalculatorController> _logger;
+        private readonly ILogger<PersonController> _logger;
 
-        public CalculatorController(ILogger<CalculatorController> logger)
+        public PersonController(ILogger<PersonController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
-
             return BadRequest("Invalid Input");
-
         }
+
         private bool IsNumeric(string strNumber)
         {
             double number;
